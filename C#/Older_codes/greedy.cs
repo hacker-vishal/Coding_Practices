1e7c125dcc15065512f4399e81315b0bf8e3e0ab
Vishal <vishalgupta.dac@gmail.com>
2021-04-24 13:46:11 +0530

    C# codes pushed

---
+++ C#/Older_codes/greedy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO.Pipes;
+
+namespace Assign.net
+{
+    class greed
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 4; i < 1000; i++)
+            {
+                int a = i;
+                int m = i;
+                int ans = 0;
+                if (m == ((3*(a / 3)) + 1))
+                {
+                    m = (2 * m) / 3;
+                    a = m;
+                    if(m == ((3 * (a / 3)) + 1))
+                    {
+                        m = (2 * m) / 3;
+                        a = m;
+                        if((m == (3 * (a / 3)) + 1))
+                        {
+                            m = (2 * m) / 3;
+                            a = m;
+                            if (m==(3*(a/3)))
+                            {
+                                ans = i;
+                                Console.WriteLine(ans);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C#/Older_codes/primes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Assign.net
+{
+    class primes
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Num 1: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Num 2: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int i, flag;
+
+            Console.WriteLine("Prime numbers from " + a + " to " + b + " are: ");
+
+            while (a < b)
+            {
+                if (a != 0 && a != 1)
+                {
+                    flag = 0;
+
+                    for (i = 2; i <= a / 2; ++i)
+                    {
+                        if (a % i == 0)
+                        {
+                            flag = 1;
+                            break;
+                        }
+                    }
+
+                    if (flag == 0)
+                        Console.WriteLine(a);
+                }
+                ++a;
+            }
+        }
+    }
+}
