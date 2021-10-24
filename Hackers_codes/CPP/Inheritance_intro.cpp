5cb5bf2380a66fc6bc02ee15b4a3461e05dacdbe
Vishal Gupta <vishagpt@gmail.com>
2021-10-24 23:28:08 +0530

    Inheritance intro done

---
+++ Hackers_codes/CPP/Inheritance_intro.cpp
@@ -0,0 +1,48 @@
+/*
+Write the code for description method in order to get the code completed and to get following output
+
+Sample Output
+
+I am an isosceles triangle
+In an isosceles triangle two sides are equal
+I am a triangle
+
+*/
+
+//Solution
+
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+class Triangle{
+    public:
+    	void triangle(){
+     		cout<<"I am a triangle\n";
+    	}
+};
+
+class Isosceles : public Triangle{
+    public:
+    	void isosceles(){
+    		cout<<"I am an isosceles triangle\n";
+    	}
+  		//Write your code here.
+        void description()
+        {
+            cout << "In an isosceles triangle two sides are equal"  << endl;
+        }
+};
+
+int main(){
+    Isosceles isc;
+    isc.isosceles();
+  	isc.description();
+    isc.triangle();
+    return 0;
+}
