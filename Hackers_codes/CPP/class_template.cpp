3ad58d93031d6701437148d2737600e9cd69eca4
Vishal Gupta <vishagpt@gmail.com>
2021-12-12 23:29:11 +0530

    class template code added

---
+++ Hackers_codes/CPP/class_template.cpp
@@ -0,0 +1,85 @@
+/*
+Create a class template to add int and float vars and concatenate strings.
+
+Sample Input
+
+3
+string John Doe
+int 1 2
+float 4.0 1.5
+Sample Output
+
+JohnDoe
+3
+5.5
+
+*/
+
+//Solution
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+using namespace std;
+
+/*Write the class AddElements here*/
+template <class T>
+class AddElements
+{
+    T element;
+public:
+    AddElements(T arg)
+    {   
+        element=arg;
+    } 
+    
+    double add(double x)
+    {
+        return element+x;
+    } 
+    
+    string concatenate(string str)
+    {
+        return element+str;
+    }
+};
+
+int start_up() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
+
+int static r = start_up();
+
+#define endl '\n';
+
+int main () {
+  int n,i;
+  cin >> n;
+  for(i=0;i<n;i++) {
+    string type;
+    cin >> type;
+    if(type=="float") {
+        double element1,element2;
+        cin >> element1 >> element2;
+        AddElements<double> myfloat (element1);
+        cout << myfloat.add(element2) << endl;
+    }
+    else if(type == "int") {
+        int element1, element2;
+        cin >> element1 >> element2;
+        AddElements<int> myint (element1);
+        cout << myint.add(element2) << endl;
+    }
+    else if(type == "string") {
+        string element1, element2;
+        cin >> element1 >> element2;
+        AddElements<string> mystring (element1);
+        cout << mystring.concatenate(element2) << endl;
+    }
+  }
+  return 0;
+}
